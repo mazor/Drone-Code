50029998ee36227e40ac1c201ff80b3aad6afb66
Sharon Mazor <sharonmazoruk@gmail.com>
2020-10-04 20:27:41 +0100

    code for a drone, summer 2015

--- drone-backend.js
+++ drone-backend.js
@@ -1,8 +1,6 @@
 var Cylon = require('cylon');
-var ws = require('nodejs-websocket');
 var bot;
 
-// Initialise the robot
 Cylon.robot()
     .connection("ardrone", {
         adaptor: 'ardrone',
@@ -12,11 +10,60 @@ Cylon.robot()
         driver: "ardrone",
         connection: "ardrone"
     })
+    .device("nav", {
+        driver: "ardrone-nav",
+        connection: "ardrone"
+    })
     .on("ready", fly);
-    
-// Fly the bot
+
 function fly(robot) {
+    bot = robot;
+    bot.drone.disableEmergency();
+    bot.drone.ftrim();
+    bot.drone.takeoff();
+    after(5*1000, function() {
+        bot.drone.right(0.4);
+    });
+
+    after(6*1000, function() {
+        bot.drone.right(0);
+        bot.drone.front(0.4);
+
+    });
+
+    after(8*1000, function() {
+        bot.drone.front(0);
+        bot.drone.left(0.4);
+
+    });
+
+    after(12*1000, function() {
+        bot.drone.left(0);
+        bot.drone.back(0.4);
+
+    });
+
+    after(14*1000, function() {
+        bot.drone.back(0);
+        bot.drone.right(0.4);
+
+    });
+
+    after(17*1000, function() {
+        bot.drone.right(0);
+        bot.drone.land();
+    });
+
+    after(22*1000, function() {
+        bot.drone.stop();
+    });
+
+    bot.nav.on("navdata", function(data) {
+        bot.drone.config('general:navdata_demo', 'TRUE');
+        console.log(data);
+    });
 
 }
 
 Cylon.start();
+
